4aee77b03ab69333615a6d96dc0ff562cdbabcdc
AlaaAlhelal <36053108+AlaaAlhelal@users.noreply.github.com>
2020-10-26 15:17:26 +0300

    Update test_flaskr.py

--- starter/backend/test_flaskr.py
+++ starter/backend/test_flaskr.py
@@ -154,7 +154,6 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['total_questions'])
-        self.assertTrue(len(data['questions']))
         self.assertTrue(len(data['categories']))
 
     ''' Test search if term is empty '''
